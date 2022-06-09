21b11db2deec0fcb789de7d82d39714253b0ab42
Yachika <yachikagupta984@gmail.com>
2022-06-10 00:23:43 +0530

    initial commit

---
+++ Datatypes/Script.js
@@ -0,0 +1,27 @@
+var x;
+console.log("x is type of "+typeof x);
+
+x = 5;
+console.log("x is type of "+typeof x);
+
+x = 6e5;
+console.log("x is type of "+typeof x);
+
+x = "jj";
+console.log("x is type of "+typeof x);
+
+x = true;
+console.log("x is type of "+typeof x);
+
+x = null;
+console.log("x is type of "+typeof x);
+
+x = {
+    firstname: "John",
+    lastname: "Doe",
+    age: 50
+}
+console.log("x is type of "+typeof x);
+
+x = ["Volvo", "BMW"]
+console.log("x is type of "+typeof x);
